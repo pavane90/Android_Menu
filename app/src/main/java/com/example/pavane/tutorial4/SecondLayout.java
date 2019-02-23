a6765ed48e87e01d175f9050a963672445e9d742
Lee, Byungman <pavane90@gmail.com>
2019-02-23 19:26:58 +0900

    Initial commit

---
+++ app/src/main/java/com/example/pavane/tutorial4/SecondLayout.java
@@ -0,0 +1,21 @@
+package com.example.pavane.tutorial4;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SecondLayout extends Fragment {
+    View v;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        v = inflater.inflate(R.layout.second_layout, container, false);
+        return v;
+        
+    }
+}
